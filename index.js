c31bec2a29c549c9dea76eea8c98ddd120f309ed
Oanh-D <128280161+Oanh-D@users.noreply.github.com>
2023-08-02 16:57:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,31 +21,31 @@ document.addEventListener("keydown", function(event){
 function keyChoice (key) {
     switch (key){
         case "w" :
-            var sound = new Audio ('sounds/tom-1.mp3');
+            var sound = new Audio ('tom-1.mp3');
             sound.play();
             break;
         case "a" :
-            var sound = new Audio ('sounds/tom-2.mp3');
+            var sound = new Audio ('tom-2.mp3');
             sound.play();
             break;
         case "s" :
-            var sound = new Audio ('sounds/tom-3.mp3');
+            var sound = new Audio ('tom-3.mp3');
             sound.play();
             break;
         case "d" :
-            var sound = new Audio ('sounds/tom-4.mp3');
+            var sound = new Audio ('tom-4.mp3');
             sound.play();
             break;
         case "j":
-            var sound = new Audio ('sounds/snare.mp3');
+            var sound = new Audio ('snare.mp3');
             sound.play();
             break;
         case "k" :
-            var sound = new Audio ('sounds/crash.mp3');
+            var sound = new Audio ('crash.mp3');
             sound.play();
             break;
         case "l":
-            var sound = new Audio ('sounds/kick-bass.mp3');
+            var sound = new Audio ('kick-bass.mp3');
             sound.play();
             break;
         default:
